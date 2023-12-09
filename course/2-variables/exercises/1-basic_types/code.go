170d09836cb44e735a5066f4e5d96a6715c83c33
Teodoro <tedtke514@gmail.com>
2023-12-09 17:53:21 -0600

    completed lesson 2.1

--- course/2-variables/exercises/1-basic_types/code.go
+++ course/2-variables/exercises/1-basic_types/code.go
@@ -4,6 +4,10 @@ import "fmt"
 
 func main() {
 	// initialize variables here
+	var username string
+	var smsSendingLimit int
+	var costPerSMS float64
+	var hasPermission bool
 
 	fmt.Printf("%v %f %v %q\n", smsSendingLimit, costPerSMS, hasPermission, username)
 }
